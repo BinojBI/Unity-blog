d3af908355c2a05482d6b7618e45689310ac97f7
MihiriKD <binojishara21@gmail.com>
2019-04-28 01:00:00 +0530

    initial commit

---
+++ Assets/scripts/player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+	public float player_speed;
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		//targetPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		
+		player_speed = Input.GetAxis("Vertical");
+		anim.SetFloat("speed",player_speed);
+			
+	}
+
+	private void LateUpdate() {
+		
+	}
+}
